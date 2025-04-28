73fb3091b8dbfede5517ce13c184b15291bb504c
Mayank Rathee <mayankrathee.japan@gmail.com>
2025-04-27 21:30:54 -0700

    Minor

--- run_docker.sh
+++ run_docker.sh
@@ -1,2 +1,6 @@
 # Run it
-docker run -it --rm -v./:/app -p 4000:4000 alfolio-site
+docker run -it --rm -v./:/app -p 4000:4000 -e GIT_AUTHOR_NAME="Mayank Rathee" \
+  -e GIT_AUTHOR_EMAIL="mayankrathee.japan@gmail.com" \
+  -e GIT_COMMITTER_NAME="Mayank Rathee" \
+  -e GIT_COMMITTER_EMAIL="mayankrathee.japan@gmail.com" \
+   alfolio-site
